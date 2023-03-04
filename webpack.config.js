18ead68a7f704744f334aee3d3519de3f5859e99
ikagengmasie <ikageng.masie@tekdigitalsolutions.co.bw>
2023-03-04 19:09:12 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 const ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = {
@@ -8,9 +8,24 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader'
-      }
-    ]
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
   resolve: {
     extensions: [
