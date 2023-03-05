5eaa391de8d3603926c5beba2ec184063489be1b
ikagengmasie <ikageng.masie@tekdigitalsolutions.co.bw>
2023-03-05 10:54:05 +0200

    added descriptions

--- src/models/base/user.ts
+++ src/models/base/user.ts
@@ -10,31 +10,31 @@ id?: number;
 @Column({type:'varchar'}) // authentication ID to be returned when creating new Firebase Auth user instance for member
 reference: string; 
     
-@Column()
+@Column({type:'varchar'})
 firstname: string;
 
-@Column()
+@Column({type:'varchar'})
 lastname: string;
 
-@Column({nullable: false})
+@Column({nullable: false,type:'varchar'})
 emailAddress: string;
 
 @Column({ nullable: false,type:'numeric'})
 createdBy: number;
  
-@Column({ nullable: false})
+@Column({ nullable: false,type:'date'})
 dateCreated: Date;
 
 @Column({ nullable: false,type:'numeric'})
 lastUpdatedBy: number;
 
-@Column({ nullable: false})
+@Column({ nullable: false,type:'date'})
 lastUpdated: Date;
 
-@Column({ nullable: false})
+@Column({ nullable: false,type:'bool'})
 active: boolean;
 
-@Column({ nullable: false})
+@Column({ nullable: false, type:'bool'})
 published: boolean;
 
 @ManyToOne(type => UserRole, {nullable:false})
